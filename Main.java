1c4ca81bd73d179a7ac68d87ecab23c5bd86b732
dackyy <eorl4940@naver.com>
2019-09-09 13:56:50 +0900

    main 클래스 틀 구현

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+/**
+ * Main 클래스
+ * 
+ * @author (2018315026 정대기, 2018315017 임민택, 2018315038 이혜인) 
+ * @version (2019.09.02)
+ */
+public class Main
+{
+    public static void main(String[] args){
+        Circle c1 = new Circle(2, 3, 5);
+        Circle c2 = new Circle(2, 3, 10);
+        System.out.println("원1 : " + c1);
+        System.out.println("원2 : " + c2);
+        if((c1).equals(c2)){
+            System.out.print("같은 원");
+        }
+        else{
+            System.out.println("서로 다른 원");
+        }
+    }
+}
