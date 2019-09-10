0d4f12c99d96dd81ddb5858f1f99c71fb0f2899f
Lim Mintaek <lmtna99@gmail.com>
2019-09-10 23:07:50 +0900

    Circle 클래스의 오버라이딩한 equals메소드 구문 수정과 Main 클래스의 if문 조건 변경

--- base/Circle.java
+++ base/Circle.java
@@ -11,8 +11,8 @@ import MyInterface.Shape;
 public class Circle implements Shape
 {
     private int radius;
-    private int x;
-    private int y;
+    public int x;
+    public int y;
 
     public Circle(int x, int y, int radius)
     {
@@ -30,7 +30,21 @@ public class Circle implements Shape
         System.out.println("반지름이 " + this.radius + "인 원입니다.");
     }
     
-    public boolean equals(Object obj){
-        
+    public Integer get_x(){
+        Integer x = new Integer(this.x);
+        return x;
+    }
+    
+    public Integer get_y(){
+        Integer y = new Integer(this.y);
+        return y;
+    }
+    
+    public boolean equals(Object obj1, Object obj2){
+        if (obj1 == obj2){
+            return true;
+        }
+        else
+            return false;
     }
 }
